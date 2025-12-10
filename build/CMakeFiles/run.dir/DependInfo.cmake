ef1dc7793b3c270c34141325a22ea0c43c685af9
Gabriel Botogoske <gabrielbotogoske@gmail.com>
2025-12-10 14:45:43 +0100

    start

---
+++ build/CMakeFiles/run.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gabriel/Documents/cherenkov-module/main.cc" "CMakeFiles/run.dir/main.cc.o" "gcc" "CMakeFiles/run.dir/main.cc.o.d"
+  "/home/gabriel/Documents/cherenkov-module/src/ActionInitialization.cc" "CMakeFiles/run.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/run.dir/src/ActionInitialization.cc.o.d"
+  "/home/gabriel/Documents/cherenkov-module/src/DetectorConstruction.cc" "CMakeFiles/run.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/run.dir/src/DetectorConstruction.cc.o.d"
+  "/home/gabriel/Documents/cherenkov-module/src/EventAction.cc" "CMakeFiles/run.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/run.dir/src/EventAction.cc.o.d"
+  "/home/gabriel/Documents/cherenkov-module/src/PrimaryGeneratorAction.cc" "CMakeFiles/run.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/run.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/gabriel/Documents/cherenkov-module/src/RunAction.cc" "CMakeFiles/run.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/run.dir/src/RunAction.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ main.cc
@@ -0,0 +1,85 @@
+//#include "DetectorConstruction.hh"
+#include "ActionInitialization.hh"
+//#include "PhysicsList.hh"
+
+#include "FTFP_BERT.hh"
+#include "G4EmStandardPhysics_option4.hh"
+#include "G4OpticalPhysics.hh"
+
+#include "G4RunManager.hh"
+
+#include "G4UImanager.hh"
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+
+#include "Randomize.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+int main(int argc,char** argv)
+{
+    // Detect interactive mode (if no arguments) and define UI session
+    //
+    G4UIExecutive* ui = 0;
+    if ( argc == 1 ) {
+    ui = new G4UIExecutive(argc, argv);
+    }
+
+    // Choose the Random engine
+    G4Random::setTheEngine(new CLHEP::RanecuEngine);
+    G4long seed = time(nullptr);    
+    G4Random::setTheSeed(seed);
+
+    // Construct the default run manager
+    //
+
+    G4RunManager* runManager = new G4RunManager;
+
+
+    // Set mandatory initialization classes
+    //
+    // Detector construction
+    //runManager->SetUserInitialization(new DetectorConstruction());
+
+    // Physics list
+
+    //auto physicsList = new PhysicsList;
+    G4VModularPhysicsList* physicsList = new FTFP_BERT;
+    physicsList->ReplacePhysics(new G4EmStandardPhysics_option4());
+    auto opticalPhysics = new G4OpticalPhysics();
+    physicsList->RegisterPhysics(opticalPhysics);
+    runManager->SetUserInitialization(physicsList);  
+    // User action initialization
+    runManager->SetUserInitialization(new ActionInitialization());
+
+    // Initialize visualization
+    //
+    G4VisManager* visManager = new G4VisExecutive;
+    visManager->Initialize();
+
+    // Get the pointer to the User Interface manager
+    G4UImanager* UImanager = G4UImanager::GetUIpointer();
+
+    // Process macro or start UI session
+    //
+    if ( ! ui ) { 
+        // batch mode
+        G4String command = "/control/execute ";
+        G4String fileName = argv[1];
+        UImanager->ApplyCommand(command+fileName);
+    }
+    else 
+    { 
+        // interactive mode
+        UImanager->ApplyCommand("/control/execute init_vis.mac");
+        //UImanager->ApplyCommand("/control/execute gui.mac");
+        //UImanager->ApplyCommand("/vis/open OGL 600x600-0+0");
+        ui->SessionStart();
+        delete ui;
+    }
+
+    delete visManager;
+    delete runManager;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo.....
